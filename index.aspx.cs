77aa82cd96cc9fdaf1ee7927d6df32bcb19a2c70
Ibragimov Ruslan <rribragimov@maxus.ru>
2015-12-07 18:41:50 +0300

    охраняю куки чтобы не вводить одно и то же

--- index.aspx.cs
+++ index.aspx.cs
@@ -13,7 +13,23 @@ using Ozeki.VoIP.SDK;
 public partial class index : System.Web.UI.Page
 {
 
-  private static CallHandlerSample _callHandlerSample;
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if (Request.Cookies["sipServerName"] != null)
+			tbSIPServer.Text = Server.HtmlEncode(Request.Cookies["sipServerName"].Value);
+		if (Request.Cookies["sipUserName"] != null)
+			tbUserName.Text = Server.HtmlEncode(Request.Cookies["sipUserName"].Value);
+		//if (Request.Cookies["sipUserPassword"] != null)
+		//	tbUserPassword.Text = Server.HtmlEncode(Request.Cookies["sipUserPassword"].Value);
+		if (Request.Cookies["sipSourceCallerID"] != null)
+			tbSourceCallerID.Text = Server.HtmlEncode(Request.Cookies["sipSourceCallerID"].Value);
+		if (Request.Cookies["sipTargetCallerID"] != null)
+			tbTargetCallerID.Text = Server.HtmlEncode(Request.Cookies["sipTargetCallerID"].Value);
+	}
+
+	private static CallHandlerSample _callHandlerSample;
+
+	
 
 	protected void AddLog(string text)
 	{
@@ -24,6 +40,12 @@ public partial class index : System.Web.UI.Page
 
 	protected void btnStartCall_Click(object sender, EventArgs e)
 	{
+		Response.Cookies["sipServerName"].Value = tbSIPServer.Text;
+		Response.Cookies["sipUserName"].Value = tbUserName.Text;
+		//Response.Cookies["sipUserPassword"].Value = tbUserPassword.Text;
+		Response.Cookies["sipSourceCallerID"].Value = tbSourceCallerID.Text;
+		Response.Cookies["sipTargetCallerID"].Value = tbTargetCallerID.Text;
+
 		if (!string.IsNullOrEmpty(tbSIPServer.Text) && !string.IsNullOrEmpty(tbUserName.Text) && !string.IsNullOrEmpty(tbSourceCallerID.Text) && //tbSourceCallerID - необязательный параметр!
 			!string.IsNullOrEmpty(tbUserPassword.Text) && !string.IsNullOrEmpty(tbTargetCallerID.Text))
 		{
